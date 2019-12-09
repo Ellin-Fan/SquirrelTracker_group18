4382f57aa4a1aea5380afff77e5d11c23a301bbe
Ellin Fan <fanxiuqi1997@gmail.com>
2019-12-09 01:51:19 +0000

    Modified export.py and make adjustments to the html pages.

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -9,11 +9,66 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with open(options['path'][0],'w') as fp:
-            fp.write('X,Y,Unique Squirrel ID,Shift,Date,Age,Primary Fur Color,Location,Specific Location,Running,Chasing,Climbing,Eating,Foraging,Other Activities,Kuks,Quaas,Moans,Tail flags,Tail twitches,Approaches,Indifferent,Runs_from\n')
+            squirrel_writer = csv.DictWriter(
+                    fp, 
+                    delimiter=',',
+                    fieldnames=[
+                        'X',
+                        'Y',
+                        'Unique Squirrel ID',
+                        'Shift',
+                        'Date',
+                        'Age',
+                        'Primary Fur Color',
+                        'Location',
+                        'Specific Location',
+                        'Running',
+                        'Chasing',
+                        'Climbing',
+                        'Eating',
+                        'Foraging',
+                        'Other Activities',
+                        'Kuks',
+                        'Quaas',
+                        'Moans',
+                        'Tail flags',
+                        'Tail twitches',
+                        'Approaches',
+                        'Indifferent',
+                        'Runs_from'
+                        ]
+                    )
+            squirrel_writer.writeheader()
+    
             for row in Sight.objects.all():
                 if row.Date: 
                     date_form = row.Date.strftime('%m%d%Y')
                 else:
                     date_form = row.Date
-                new_line = f"{row.Longitude},{row.Latitude},{row.Unique_Squirrel_Id},{row.Shift},{date_form},{row.Age},{row.Primary_Fur_Color},{row.Location},{row.Specific_Location},{row.Running},{row.Chasing},{row.Climbing},{row.Eating},{row.Foraging},{row.Other_Activities},{row.Kuks},{row.Quaas},{row.Moans},{row.Tail_Flags},{row.Tail_Twitches},{row.Approaches},{row.Indifferent},{row.Runs_From},\n"
-                fp.write(new_line)
+
+                squirrel_writer.writerow({
+                    'X':row.Longitude,
+                    'Y':row.Latitude,
+                    'Unique Squirrel ID':row.Unique_Squirrel_Id,
+                    'Shift':row.Shift,
+                    'Date':date_form,
+                    'Age':row.Age,
+                    'Primary Fur Color':row.Primary_Fur_Color,
+                    'Location':row.Location,
+                    'Specific Location':row.Specific_Location,
+                    'Running':row.Running,
+                    'Chasing':row.Chasing,
+                    'Climbing':row.Climbing,
+                    'Eating':row.Eating,
+                    'Foraging':row.Foraging,
+                    'Other Activities':row.Other_Activities,
+                    'Kuks':row.Kuks,
+                    'Quaas':row.Quaas,
+                    'Moans':row.Moans,
+                    'Tail flags':row.Tail_Flags,
+                    'Tail twitches':row.Tail_Twitches,
+                    'Approaches':row.Approaches,
+                    'Indifferent':row.Indifferent,
+                    'Runs_from':row.Runs_From,          
+                    })
+
